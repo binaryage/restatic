cf649aafe104fc3be302115721fedf4f2ed31ccb
Jan Palounek <jan@palounek.com>
2012-02-14 18:07:40 +0100

    YAML formated config file loading implemented, YAML parser added to libs.

--- parser.php
+++ parser.php
@@ -2,5 +2,20 @@
 
 // var_dump($_SERVER['argv']);
 
+require_once 'libs/spyc-0.5/spyc.php';
+define("CONF_FILE", 'goopages.yml');
+
 $args = $_SERVER['argv'];
+$source = $args[1];
+$target = $args[2];
+
+// @todo Check if last char is not /
+if(file_exists($source . '/' . 'goopages.yml')) {
+	$config = Spyc::YAMLLoad($source . '/' . CONF_FILE);
+} else {
+	die("Config file doesnt exists");
+}
+
+var_dump($config);
+
 echo 'Source dir ' . $args[1] . ' target dir ' . $args[2] . PHP_EOL;
