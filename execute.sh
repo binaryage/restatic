414f6a0cd420237d4215599aca33487ec69a30c9
Jan Palounek <jan@palounek.com>
2012-02-14 17:33:33 +0100

    Basic tool structure implemented.

---
+++ execute.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+if [ $# -lt 1 ] ; then
+        echo "Usage: parse google docs spreadsheet content using <COMMAND> source_folder target_folder"
+        echo 
+        echo "By Binaryage.com, for more info see projects homepage - goopages.binaryage.com or github wiki"
+        echo " git@github.com:JPalounek/goopages.git"
+        exit 1
+fi
+
+echo "Goopages started parsing html files from $1 to $2."
+
+if [ -d $1 ]
+then
+	if [ -d $1 ]
+	then
+		php parser.php $1 $2
+	else
+		echo "Target dir doesnt exists."
+	fi
+else
+ echo "Source dir doesnt exists."
+fi
---
+++ parser.php
@@ -0,0 +1,6 @@
+<?php
+
+// var_dump($_SERVER['argv']);
+
+$args = $_SERVER['argv'];
+echo 'Source dir ' . $args[1] . ' target dir ' . $args[2] . PHP_EOL;
