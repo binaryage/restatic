ef45adb3e571dedd65322bab9ab12107acb08998
Jan Palounek <jan@palounek.com>
2012-02-15 18:32:11 +0100

    Refactored.

--- GooDataExtractor.php
+++ GooDataExtractor.php
@@ -16,22 +16,6 @@ class GooDataExtractor {
 		return $result;		
 	}
 
-	/*
-	protected static function csvToArray($file, $delimiter) { 
-		if (($handle = fopen($file, 'r')) !== FALSE) { 
-			$i = 0; 
-			while (($lineArray = fgetcsv($handle, 4000, $delimiter, '"')) !== FALSE) { 
-				for ($j = 0; $j < count($lineArray); $j++) { 
-					$arr[$i][$j] = $lineArray[$j]; 
-				} 
-      			$i++; 
-    		} 
-    		fclose($handle); 
-  		} 
-  		return $arr; 
-	} 
-	*/
-
 	protected static function csvToArray($file, $delimiter) { 
 		echo '------------------------------------------------------------------------' . PHP_EOL;
 		var_dump($file);
@@ -39,15 +23,15 @@ class GooDataExtractor {
 	}
 
 	public static function parseContentToArray($key, $sheets) {
-		// return self::csvToArray($link, ',');
 		$sheets = self::mineData($key, $sheets);
+		$data = array();
+
 		if(is_array($sheets)) {
 			foreach($sheets as $sheet) {
-				self::csvToArray($sheet, ',');
+				$data[] = self::csvToArray($sheet, ',');
 			}
 		}
 
-		// foreach
-		// return 
+		return $data;
 	}
 }
