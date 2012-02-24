4f432be077ee5b36f16d361983da25d8f3a1d873
Jan Palounek <jan@palounek.com>
2012-02-24 13:51:59 +0100

    Installation script bugfix.

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
 cd ~/bin
 mkdir restatic
 cd restatic
-wget https://github.com/JPalounek/restatic/tarball/v0.1
-tar -xf v0.1
+wget https://github.com/JPalounek/restatic/tarball/v0.2
+tar -xf v0.2
 mv JPalounek-restatic*/* .
 
 rm -rf v0.1
@@ -10,5 +10,3 @@ rm -rf JPalounek-restatic*/
 
 cd /usr/sbin
 link ~/bin/restatic/restatic restatic
-
-# wget --no-check-certificate https://github.com/JPalounek/restatic/tarball/master
