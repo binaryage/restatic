efce8e8afe496c97a398d1ffc17ee4cedb7ddc0f
Jan Palounek <jan@palounek.com>
2012-03-01 18:05:25 +0100

    Install improved.

--- install.sh
+++ install.sh
@@ -1,12 +1,16 @@
-cd ~/bin
-mkdir restatic
-cd restatic
+# Create a directory
+mkdir ~/.restatic
+cd ~/.restatic
+
+# Download latest stabile version
 wget https://github.com/JPalounek/restatic/tarball/v0.2
 tar -xf v0.2
 mv JPalounek-restatic*/* .
 
-rm -rf v0.1
+# Sweep after operations
+rm -rf v0.2
 rm -rf JPalounek-restatic*/
 
+# Link bash to callable command
 cd /usr/sbin
-link ~/bin/restatic/restatic restatic
+link ~/.restatic/restatic restatic
