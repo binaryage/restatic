179315672b5164274e063b86ff9f57d0bf4615db
Jan Palounek <jan@palounek.com>
2012-04-07 00:34:55 +0200

    Bash script functionality moved to node (parser.js).

--- parser.js
+++ parser.js
@@ -1,3 +1,4 @@
+/* ---- Variables setup ---- */
 var GooDataExtractor = require('./GooDataExtractor.js');
 var SiteParser = require('./SiteParser.js');
 
@@ -10,49 +11,83 @@ var fs = require('fs');
 var arguments = process.argv.splice(2);
 var source = arguments[0];
 var target = arguments[1];
+var checked = true;
 
-if(source.charAt(source.length - 1) == '/') {
-  source = source.slice(0, -1);
+/* ---- Input vars checking ---- */
+if(typeof target == 'undefined') {
+  if(typeof source == 'undefined') {
+    console.log("\033[32mUsage: parse google docs spreadsheet content using 'restatic source_folder target_folder' or \'restatic -d\' if you want to generate site from actual location to folder _site '\033[39m");
+    console.log("By Binaryage.com, for more info see projects homepage");
+    checked = false;
+  }
 }
 
-var config = source + '/restatic.yml';
-var optimized = '';
+try {
+  sourceStats = fs.lstatSync(source);
+}
 
-var fileContents = fs.readFile(config, function(err, fileContents) {
-  var yaml = require('yaml');
-  fileContents = fileContents.toString();
-  fileContents = '\n' + fileContents;
+catch(e) {
+  if(source != '-d') {
+    console.log('Source directory doesn\'t exists.');
+    checked = false;
+  }
+}
 
-  var commentedLine = false;
+try {
+  targetStats = fs.lstatSync(target);
+}
 
-  for (var i = 0; i <= fileContents.length; i++) {
-    if(typeof fileContents[i + 1] != 'undefined') {
-      if(fileContents[i] != '\n') {
-        if(commentedLine != true) {
-          optimized = optimized + fileContents[i];
-        }
-      } else {
-        if(fileContents[i + 1] == '#') {
-          commentedLine = true;
+catch(e) {
+  console.log('Target directory doesn\'t exists.');
+  checked = false;
+}
+
+/* ---- Restatic ---- */
+if(checked) {
+  console.log('\033[31mRestatic started parsing html files from ../source_folder_example/ to ../target_folder_example/.\033[39m');
+  if(source.charAt(source.length - 1) == '/') {
+    source = source.slice(0, -1);
+  }
+  
+  var config = source + '/restatic.yml';
+  var optimized = '';
+  
+  var fileContents = fs.readFile(config, function(err, fileContents) {
+    var yaml = require('yaml');
+    fileContents = fileContents.toString();
+    fileContents = '\n' + fileContents;
+  
+    var commentedLine = false;
+  
+    for (var i = 0; i <= fileContents.length; i++) {
+      if(typeof fileContents[i + 1] != 'undefined') {
+        if(fileContents[i] != '\n') {
+          if(commentedLine != true) {
+            optimized = optimized + fileContents[i];
+          }
         } else {
-          commentedLine = false;
+          if(fileContents[i + 1] == '#') {
+            commentedLine = true;
+          } else {
+            commentedLine = false;
+          }
         }
       }
     }
-  }
-
-  optimized = yaml.eval(optimized);
-  var key = optimized.googleSpreadSheetKey;
-  var delimiters = optimized.delimiter;
-/*
-  delimiters = delimiters.replace(" ", "");
-  delimiters = delimiters.split(",");
-*/
-
-/*
-  var importedData = GooDataExtractor.extract(key, delimiters);
-  console.log(importedData);
-*/
-
-  GooDataExtractor.extract(key, delimiters, SiteParser.parse);
-});
+  
+    optimized = yaml.eval(optimized);
+    var key = optimized.googleSpreadSheetKey;
+    var delimiters = optimized.delimiter;
+  /*
+    delimiters = delimiters.replace(" ", "");
+    delimiters = delimiters.split(",");
+  */
+  
+  /*
+    var importedData = GooDataExtractor.extract(key, delimiters);
+    console.log(importedData);
+  */
+  
+    GooDataExtractor.extract(key, delimiters, SiteParser.parse);
+  });
+}
