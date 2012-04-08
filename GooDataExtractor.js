e21aa26a149d823d83e77ea683d89c8c95a25a42
Jan Palounek <jan@palounek.com>
2012-04-08 03:25:44 +0200

    Content parsing implemented.

--- GooDataExtractor.js
+++ GooDataExtractor.js
@@ -28,42 +28,41 @@ GooDataExtractor.rawData = '';
 GooDataExtractor.sheetNumber = 1;
  
 GooDataExtractor.prototype.mineData = function (key, sheet, callback) {
-        var http = require('http');
+	var http = require('http');
 
-        var state = '';
-        var rawData;
+	var state = '';
+	var rawData;
 
-		var link = '/feeds/cells/' + key + '/' + sheet + '/public/values';
+	var link = '/feeds/cells/' + key + '/' + sheet + '/public/values';
  
-		var options = {
-			host: 'spreadsheets.google.com',
-			port: 80,
-			path: link
-		};
+	var options = {
+		host: 'spreadsheets.google.com',
+		port: 80,
+		path: link
+	};
 
-		http.get(options, function(res) {
-			res.setEncoding('utf-8');
+	http.get(options, function(res) {
+		res.setEncoding('utf-8');
 
-			res.on('data', function (chunk) {
-				rawData += chunk;
-			});
+		res.on('data', function (chunk) {
+			rawData += chunk;
+		});
 	 
-			res.on('end', function () {
-				if(rawData.length < 60) {
-					if(GooDataExtractor.rawData.substring(0, 9) == 'undefined') {
-						GooDataExtractor.rawData = GooDataExtractor.rawData.substring(9, GooDataExtractor.rawData.length);
-					}
-
-					console.log(GooDataExtractor.rawData);
-					// callback(GooDataExtractor.rawData);
-				} else {
-					GooDataExtractor.rawData += rawData;
-					GooDataExtractor.sheetNumber++;
-					GooDataExtractor.prototype.mineData(key, GooDataExtractor.sheetNumber);
-					rawData = '';
+		res.on('end', function () {
+			if(rawData.length < 60) {
+				if(GooDataExtractor.rawData.substring(0, 9) == 'undefined') {
+					GooDataExtractor.rawData = GooDataExtractor.rawData.substring(9, GooDataExtractor.rawData.length);
 				}
-			});
-		})
+				console.log(GooDataExtractor.rawData);
+				// callback(GooDataExtractor.rawData);
+			} else {
+				GooDataExtractor.rawData += rawData;
+				GooDataExtractor.sheetNumber++;
+				GooDataExtractor.prototype.mineData(key, GooDataExtractor.sheetNumber);
+				rawData = '';
+			}
+		});
+	})
 }
 
 module.exports = GooDataExtractor;
--- SiteParser.js
+++ SiteParser.js
@@ -6,17 +6,48 @@ SiteParser.dirs = new Array();
 /* Only public function called from outside of SiteParser */
 SiteParser.prototype.parse = function (data, target) {
 	var fs = require('fs');
+	var i = 0;
+	var j = 0;
+	SiteParser.prototype.prepareData(target);
+
+	SiteParser.filesToParse.forEach(function (file) {
+		fs.readFile(file, 'utf8', function (err, content) {
+			j++;
+			for(var key in data) {
+				updated = content.replace(key, data[key]);
+
+				if(content != updated) {
+					fs.writeFileSync(file, updated, 'utf8');
+					console.log('Replacing ' + key + ' with ' + data[key] + ' in ' + file.replace(target, ''));
+					i++;
+				}
+			}
+
+			if(i == 0) {
+				console.log('Nothing to update in ' + file);
+			} else {
+				i = 0;
+			}
+
+			if(j == SiteParser.filesToParse.length) {
+				console.log('\033[32mParsing done, enjoy it! Its prepared in \033[31m' + target + '\033[39m');
+			}
+		});
+	});
+}
+
+SiteParser.prototype.prepareData = function (target) {
+	var fs = require('fs');
 	var dir = fs.readdirSync(target);
 
 	SiteParser.dirs[0] = target;
 	SiteParser.prototype.walkThrought(dir, target);
 
 	SiteParser.dirs.forEach(function (dir) {
-		SiteParser.prototype.indexDir(fs.readdirSync(dir));
+		SiteParser.prototype.indexDir(fs.readdirSync(dir), dir);
 	});
-
-	console.log(SiteParser.filesToParse);
 }
+
 /* Tested for two levels of depth */
 SiteParser.prototype.walkThrought = function (dir, path) {
 	var fs = require('fs');
@@ -42,16 +73,20 @@ SiteParser.prototype.walkThrought = function (dir, path) {
 }
 
 /* Finds all .html or .htm ending files */
-SiteParser.prototype.indexDir = function (dir) {
+SiteParser.prototype.indexDir = function (dir, path) {
 	var i = SiteParser.filesToParse.length;
+
+	if(path.charAt(path.length - 1) == '/') {
+    	path = path.slice(0, -1);
+  	}
 
 	dir.forEach(function(file) {
 		if(file.substr(-5) == '.html') {
-			SiteParser.filesToParse[i] = file;
+			SiteParser.filesToParse[i] = path + '/' + file;
 			i++;
 		} else {
 			if(file.substr(-4) == '.htm') {
-				SiteParser.filesToParse[i] = file;
+				SiteParser.filesToParse[i] = path + '/' + file;
 				i++;
 			}
 		}
