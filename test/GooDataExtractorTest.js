d70e578c9ed274d4ea529f1b6480a323f12fec45
Jan Palounek <jan@palounek.com>
2012-04-19 12:17:44 +0200

    Prepared for writing tests.

---
+++ test/GooDataExtractorTest.js
@@ -0,0 +1,11 @@
+var Extractor = require('../GooDataExtractor.js');
+var Extractor = new Extractor();
+
+// Not working yet
+describe('Extractor', function(){
+  describe('#extract()', function(){
+    it('should return Object', function(done){
+      var result = Extractor.extract(key, delimiters, target, callback);
+    })
+  })
+})
